fd72d56af64798cef887988955e45d7406b51e8b
nikhilchandurkar <nikhilchandurkar24@gamil.com>
2024-12-29 10:06:39 +0530

    "config modified"

--- server/constants/config.js
+++ server/constants/config.js
@@ -2,9 +2,10 @@
 
 const corsOption = {
     origin: [
-        // "http://localhost:5173",
+        "http://localhost:5173",
         // "http://localhost:4173",
-        
+        'https://chat-app-client-lime-three.vercel.app/',
+        'https://chat-app-api-wine.vercel.app/',
         process.env.CLIENT_URL,
         process.env.NODE_ENV === 'production' 
     ],
